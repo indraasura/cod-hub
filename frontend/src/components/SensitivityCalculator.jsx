5c15d3e267b61e558585832093aeca67cfb42476
Swarup Hegde <swaruphegde@gmail.com>
2020-08-06 19:39:27 +0530

    feat: server

--- frontend/src/components/SensitivityCalculator.jsx
+++ frontend/src/components/SensitivityCalculator.jsx
@@ -7,27 +7,27 @@ class Calculator extends Component {
       this.props.standardSensitivity === 0 ? (
         <div></div>
       ) : (
-        <div className="count-values">
-          <h3>Firing Sensitivity: </h3>
-          <p>{this.props.standardSensitivity * 2}</p>
-          <br />
-          <h3>Gyro Sensitivity: </h3>
-          <p>{this.props.standardSensitivity * 0.75}</p>
-          <br />
-          <h3>Sinper Sensitivity: </h3>
-          <p>{this.props.standardSensitivity * 1.5}</p>
-          <br />
-          <h3>XYZ Sensitivity: </h3>
-          <p>{this.props.standardSensitivity * 1.25}</p>
-        </div>
-      );
+          <div className="count-values">
+            <h3>Firing Sensitivity: </h3>
+            <p>{this.props.standardSensitivity * 2}</p>
+            <br />
+            <h3>Gyro Sensitivity: </h3>
+            <p>{this.props.standardSensitivity * 0.75}</p>
+            <br />
+            <h3>Sinper Sensitivity: </h3>
+            <p>{this.props.standardSensitivity * 1.5}</p>
+            <br />
+            <h3>XYZ Sensitivity: </h3>
+            <p>{this.props.standardSensitivity * 1.25}</p>
+          </div>
+        );
     return (
       <div className="calculator">
         <div className="container">
           <div className="value-container">
             <form>
               <label>Standard Sensitivity</label>
-              <br/><br/>
+              <br /><br />
               <input
                 type="text"
                 onChange={this.props.handleSensitivityChange}
@@ -38,6 +38,12 @@ class Calculator extends Component {
             <br />
             {sensitivityValues}
           </div>
+        </div>
+        <div className="text-container">
+          <h1>Sensitivity Calculator</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Quo sit sed sequi alias minus et earum nisi magnam amet enim repellendus, explicabo deserunt quam 
+            doloremque aperiam nam eaque autem dolorum.</p>
         </div>
       </div>
     );
